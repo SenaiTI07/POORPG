2f96164d10a75226d05694d7f2268506cd8ab45c
Marcos A <macosaures@gmail.com>
2018-09-11 16:46:14 -0300

    Atualização marcão!

--- POORPG/src/poorpg/AbstractPersonagem.java
+++ POORPG/src/poorpg/AbstractPersonagem.java
@@ -24,50 +24,64 @@ public abstract class AbstractPersonagem
         this.nome = nome;
     }
 
+    @Override
+    public boolean atacar(int atr1, int atr2) {
+        int aux = (int)(atr1 / 10) + (atr2 / 10);
+        if (1 + aux + (int)Math.random() * (100 - aux - 1) > 50) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int getHP() {
+        return this.hp;
+    }
+
+    @Override
+    public void setHP(int hp) {
+        this.hp = hp;
+    }
+    
+    @Override
     public int getTipo() {
         return tipo;
     }
-
+    
+    @Override
     public void setTipo(int tipo) {
         this.tipo = tipo;
     }
-
-    public int getHp() {
-        return hp;
-    }
-
-    public void setHp(int hp) {
-        this.hp = hp;
-    }
-
+    @Override
     public int getForca() {
         return forca;
     }
-
+    @Override
     public void setForca(int forca) {
         this.forca = forca;
     }
-
+    @Override
     public int getDestreza() {
         return destreza;
     }
-
+    @Override
     public void setDestreza(int destreza) {
         this.destreza = destreza;
     }
-
+    @Override
     public int getInteligencia() {
         return inteligencia;
     }
-
+    @Override
     public void setInteligencia(int inteligencia) {
         this.inteligencia = inteligencia;
     }
-
+    @Override
     public String getNome() {
         return nome;
     }
-
+    @Override
     public void setNome(String nome) {
         this.nome = nome;
     }
