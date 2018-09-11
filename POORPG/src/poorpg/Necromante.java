49265f93714476ce7de7c06fffb0b1436951cac8
MateusViniciusNogueira <mateus_nogueira98@hotmail.com>
2018-09-11 17:10:16 -0300

    Necromante
    .

---
+++ POORPG/src/poorpg/Necromante.java
@@ -0,0 +1,14 @@
+package poorpg;
+
+public class Necromante extends AbstractPersonagem {
+
+    public Necromante(int cadaveres, int tipo, int hp, int forca, int destreza, int inteligencia, String nome) {
+        super(tipo, hp, forca, destreza, inteligencia, nome);
+
+    }
+
+    public void invocarCadaveres() {
+
+    }
+
+}
