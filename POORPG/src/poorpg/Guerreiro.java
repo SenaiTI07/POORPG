aa7e06ed0b30aa38ed9146150497575701151c7b
Luizt07 <luiz_1609@hotmail.com>
2018-09-11 16:44:43 -0300

    Guerreiro
    guerreiro

--- POORPG/src/poorpg/Guerreiro.java
+++ POORPG/src/poorpg/Guerreiro.java
@@ -11,74 +11,24 @@ package poorpg;
  */
 public class Guerreiro extends AbstractPersonagem {
 
-    public Guerreiro(int tipo, int hp, int forca, int destreza, int inteligencia, String nome) {
+    int forcaEspada;
+    int block;
+
+    public Guerreiro(int forcaEspada, int block, int tipo, int hp, int forca, int destreza, int inteligencia, String nome) {
         super(tipo, hp, forca, destreza, inteligencia, nome);
+        this.forcaEspada = forcaEspada;
+        this.block = block;
     }
+    
+    
 
     @Override
     public boolean atacar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       return true;
     }
 
-   
-    public String getNome(String nome) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setNome(String nome) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-  
-    public int getHP(int hp) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    public void setHP() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-   
-    public int getForca(int forca) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    public void setForca() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    public int getDestreza(int destreza) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    public void setDestreza() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    public int getInteligencia(int inteligencia) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    public void setInteligencia() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    public int getTipo(int tipo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-    public void setTipo() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+ 
+ 
 
 
     
