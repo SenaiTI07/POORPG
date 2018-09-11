4f092a72e7806d9c829a29635e5f7f7215abc9cf
Marcos A <macosaures@gmail.com>
2018-09-11 17:23:24 -0300

    123

--- POORPG/src/poorpg/Paladino.java
+++ POORPG/src/poorpg/Paladino.java
@@ -11,7 +11,9 @@ package poorpg;
  */
 public class Paladino extends AbstractPersonagem {
 
-    public Paladino(int tipo, int hp, int forca, int destreza, int inteligencia, String nome) {
-        super(tipo, hp, forca, destreza, inteligencia, nome);
+    public Paladino(int tipo, int hp, int forca, int destreza, int inteligencia, int level, String nome) {
+        super(tipo, hp, forca, destreza, inteligencia, level, nome);
     }
+    
+    
 }
