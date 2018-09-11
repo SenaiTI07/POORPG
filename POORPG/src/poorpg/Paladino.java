e9db6657a19b1cfd3756f2bd1922b3217bda0361
Guilherme Sanches <guiisanches123@hotmail.com>
2018-09-11 17:13:17 -0300

    a
    a

--- POORPG/src/poorpg/Paladino.java
+++ POORPG/src/poorpg/Paladino.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package poorpg;
 
-/**
- *
- * @author User
- */
 public class Paladino extends AbstractPersonagem {
 
     public Paladino(int tipo, int hp, int forca, int destreza, int inteligencia, int level, String nome) {
