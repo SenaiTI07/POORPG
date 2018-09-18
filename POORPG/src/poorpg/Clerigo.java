46519a7a105ea6caa8d511654ff8742244a1aa9b
Marcos A <macosaures@gmail.com>
2018-09-18 15:11:05 -0300

    asdas

--- POORPG/src/poorpg/Clerigo.java
+++ POORPG/src/poorpg/Clerigo.java
@@ -1,23 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package poorpg;
 
 import poorpg.batalha.ataque.AbstractAtaque;
 import poorpg.batalha.ataque.AtaqueMagico;
 
-/**
- *
- * @author senai
- */
 public class Clerigo extends AbstractPersonagem {
-    
+
     public Clerigo(int tipo, int hp, int forca, int destreza, int inteligencia, int level, String nome) {
         super(tipo, hp, forca, destreza, inteligencia, level, nome);
     }
-    
+
     public void Curar(AbstractPersonagem personagem) {
         personagem.setHP(personagem.getHP() + (int)(this.getInteligencia() * 3.5)) ;
     }
@@ -31,7 +22,7 @@ public class Clerigo extends AbstractPersonagem {
     public int receberAtaque(AbstractAtaque a) {
         return a.getQuantidade() - (this.getInteligencia() * (this.getDestreza() / 10));
     }
-    
-    
-    
+
+
+
 }
