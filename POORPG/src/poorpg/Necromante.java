1c1b93fbc677b7c638f916895ce964789009f64b
NewPedreiro <43181686+MateusViniciusNogueira@users.noreply.github.com>
2018-09-18 15:59:49 -0300

    Necromante
    tae

--- POORPG/src/poorpg/Necromante.java
+++ POORPG/src/poorpg/Necromante.java
@@ -1,12 +1,39 @@
 package poorpg;
 
+import poorpg.batalha.acao.AbstractAtaque;
+import poorpg.batalha.acao.AtaqueMagico;
+
 public class Necromante extends AbstractPersonagem {
 
-    public Necromante(int tipo, int hp, int forca, int destreza, int inteligencia, int level, String nome) {
-        super(tipo, hp, forca, destreza, inteligencia, level, nome);
+    public int getMana() {
+        return mana;
     }
 
-    public void invocarCadaveres() {
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
 
+    private int mana;
+
+    public Necromante(int tipo, int hp, int forca, int destreza, int inteligencia, int level, int mana, String nome) {
+        super(tipo, hp, forca, destreza, inteligencia, level, nome);
+        this.mana = mana;
+    }
+
+    public Necromante() {
+        super(NECROMANTE, 95, 5, 6, 10, 1, "Kel'Thuzad");
+        this.mana = 9;
+    }
+
+
+
+    @Override
+    public AbstractAtaque atacar() {
+        return new AtaqueMagico((this.getInteligencia() / 2) + (this.getMana() / 2));
+    }
+
+    @Override
+    public int receberAtaque(AbstractAtaque a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
