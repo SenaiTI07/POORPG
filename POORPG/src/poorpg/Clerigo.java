9baba2ff5d96774150aafe5969aaf91cf042a309
Marcos A <macosaures@gmail.com>
2018-09-18 14:43:50 -0300

    Atualização classe clerigo

--- POORPG/src/poorpg/Clerigo.java
+++ POORPG/src/poorpg/Clerigo.java
@@ -5,6 +5,9 @@
  */
 package poorpg;
 
+import poorpg.batalha.ataque.AbstractAtaque;
+import poorpg.batalha.ataque.AtaqueMagico;
+
 /**
  *
  * @author senai
@@ -18,5 +21,17 @@ public class Clerigo extends AbstractPersonagem {
     public void Curar(AbstractPersonagem personagem) {
         personagem.setHP(personagem.getHP() + (int)(this.getInteligencia() * 3.5)) ;
     }
+
+    @Override
+    public AbstractAtaque atacar() {
+        return new AtaqueMagico(this.getInteligencia() * this.getHP() / 2);
+    }
+
+    @Override
+    public int receberAtaque(AbstractAtaque a) {
+        return a.getQuantidade() - (this.getInteligencia() * (this.getDestreza() / 10));
+    }
+    
+    
     
 }
