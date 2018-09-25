e789f0908ce8c11f8be2d02f63d9a73be2a7c6dc
Luizt07 <luiz_1609@hotmail.com>
2018-09-25 14:04:13 -0300

    Guerreiro
    Carai

--- POORPG/src/poorpg/Guerreiro.java
+++ POORPG/src/poorpg/Guerreiro.java
@@ -16,49 +16,38 @@ import poorpg.batalha.acao.IAtaque;
  */
 public class Guerreiro extends AbstractPersonagem {
 
- 
-  
-
-    public Guerreiro( int tipo, int hp, int forca, int destreza, int inteligencia, int level, String nome) {
+    public Guerreiro(int tipo, int hp, int forca, int destreza, int inteligencia, int level, String nome) {
         super(tipo, hp, forca, destreza, inteligencia, level, nome);
-       
-        
+
     }
 
     @Override
-    public AbstractAtaque atacar( ) {
+    public AbstractAtaque atacar() {
 
-        return new AtaqueFisico(((getForca()/2) + (getDestreza()/2))*2);
-        
+        return new AtaqueFisico(((getForca() / 2) + (getDestreza() / 2)) * 2);
+
     }
 
     @Override
     public int receberAtaque(IAtaque a) {
-           
+
         int dano = a.getQuantidade() - this.getForca();
-        this.setHP(this.getHP() - dano); 
-        return  dano;
+        this.setHP(this.getHP() - dano);
+        return dano;
     }
 
-
-    public Guerreiro(){
-        super(GUERREIRO,100,6,6,2,5,"DesertoNegro");
+    public Guerreiro() {
+        super(GUERREIRO, 100, 6, 6, 2, 5, "DesertoNegro");
     }
 
     @Override
     public int getDef(int tipoAtaque) {
-       int defesa;
+        int defesa;
         Random math = new Random();
-            
-            defesa = this.getForca()/2+(int)math.nextInt(10)+(math.nextInt(10) * this.getDestreza());
-        
+
+        defesa = this.getForca() + (int) math.nextInt(10) + (math.nextInt(10) * this.getDestreza());
+
         return defesa;
     }
-
-
-    
-    
-
-  
 
 }
