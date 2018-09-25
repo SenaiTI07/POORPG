a3687518f0606a65bbd62f1bb67a410de86d6a4a
fernandonakagawa <42784984+fernandonakagawa@users.noreply.github.com>
2018-09-25 15:28:59 -0300

    Ladino

--- POORPG/src/poorpg/Ladino.java
+++ POORPG/src/poorpg/Ladino.java
@@ -39,6 +39,11 @@ public class Ladino extends AbstractPersonagem{
         return a.getQuantidade();
     }
 
+    @Override
+    public int getDef(int tipoAtaque) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     
    
     
